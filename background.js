add21b746f1f3db83aa14bd795ee833ac9eaab5d
cclambie <craig@whatwasthat.com.au>
2025-12-15 04:59:32 +0000

    Firefox MV2 manifest and background for AMO

--- background.js
+++ background.js
@@ -1,14 +1,14 @@
+/*
 // MV3: service worker background script
 chrome.action.onClicked.addListener(() => {
   // Works in Chrome and Firefox MV3
   chrome.runtime.openOptionsPage();
 });
+*/
 
-/*
 // Open the options page when the toolbar icon is clicked.
 browser.browserAction.onClicked.addListener(() => {
   browser.runtime.openOptionsPage().catch((error) => {
     console.error('Failed to open options page:', error);
   });
 });
-*/
